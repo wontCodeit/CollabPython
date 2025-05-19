6597aab6dbcf34252a15060e642cdc19b813c0ea
wontCodeit <wontbaker@gmail.com>
2025-05-19 20:23:30 +0100

    Bad change

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-import turtle; print("Hello, World!"); ttl1 = turtle.Turtle(); def ondpress(): ttl1.forward(50)
-try: d = open("laserCoords.txt", 'r')
+import turtle
+
+print("Hello, World!")
+ttl1 = turtle.Turtle()
+
+def ondpress():
+    ttl1.forward(50)
+
+try:    
+    d = open("laserCoords.txt", 'r')
 except FileNotFoundError:
-    ttl1.pencolor((1.0, 0, 0)); print("bozo")
-main_window = turtle.Screen(); main_window.onkeyrelease(ondpress, "d"); ttl1.forward(50); main_window.listen(); main_window.mainloop(); my_password = "Humpty Dumpty sat on a wall, Humpy Dumpty had a great fall, and all the king's horses and all the king's men couldn't put Humpty Dumpty back together again innit"
+    ttl1.pencolor((1.0, 0, 0))
+    print("bozo")
+
+
+
+main_window = turtle.Screen()
+main_window.onkeyrelease(ondpress, "d")
+ttl1.forward(50)
+main_window.listen()
+main_window.mainloop()
+my_password = "Humpty Dumpty sat on a wall, Humpy Dumpty had a great fall, and all the king's horses and all the king's men couldn't put Humpty Dumpty back together again innit"
