8f0d1018947a86ef2b9545b2608dcd221ff259a7
wontCodeit <wontbaker@gmail.com>
2025-05-19 20:08:02 +0100

    Added main template

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello, World")
+import turtle
+
+print("Hello, World!")
+ttl1 = turtle.Turtle()
+
+def ondpress():
+    ttl1.forward(50)
+
+try:    
+    d = open("laserCoords.txt", 'r')
+except FileNotFoundError:
+    ttl1.pencolor((1.0, 0, 0))
+    print("bozo")
+
+
+
+main_window = turtle.Screen()
+main_window.onkeyrelease(ondpress, "d")
+ttl1.forward(50)
+main_window.listen()
+main_window.mainloop()
